61cb61e5ecae9c2689b8ad62eff47cf3d30452d0
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2025-08-14 10:20:11 -0300

    feat: use zod to implement schema validation

--- src/controllers/sign-up-controller.ts
+++ src/controllers/sign-up-controller.ts
@@ -1,9 +1,28 @@
+import z from "zod";
 import { HttpRequest } from "../types/http-request";
 import { HttpResponse } from "../types/http-response";
-import { ok } from "../utils/http";
+import { badRequest, created } from "../utils/http";
+
+const singUpSchema = z.object({
+  goal: z.enum(['LOSE', 'MAINTAIN', 'GAIN']),
+  gender: z.enum(['MALE', 'FEMALE']),
+  birthDate: z.iso.date(),
+  height: z.number(),
+  weight: z.number(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().nonempty(),
+    email: z.email(),
+    password: z.string().min(8)
+  })
+})
 
 export class SignUpController {
-  async handle({ }: HttpRequest): Promise<HttpResponse> {
-    return ok({ accessToken: 'token de acesso'})
+  async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = singUpSchema.safeParse(body);
+    if (!success) {
+      return badRequest({ errors: error.issues })
+    }
+    return created({ data })
   }
 }
